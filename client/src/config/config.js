744f075f2c442dbafbd76e9962af6ce626af988b
onzzzzz <126831899+onzzzzz@users.noreply.github.com>
2023-06-23 14:41:54 +0100

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://shirt2.onrender.com",
+    backendUrl: "https://shirt2.onrender.com/api/v1/dalle",
   },
 };
 
